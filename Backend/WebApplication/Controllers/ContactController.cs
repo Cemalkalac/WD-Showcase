95c1e778beeae3b780cefe377a1204e55bb4f653
Cemalkalac <S1181428@student.windesheim.nl>
2024-02-09 13:51:46 +0100

    .

---
+++ Backend/WebApplication/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Me() 
+        {
+            return View(); 
+        }
+    }
+}
